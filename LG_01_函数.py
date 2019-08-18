a3ef0d9f811fcadc705e9bcd226f84051f518e75
tom <123@123>
2019-08-18 19:21:36 +0800

    提交到哪里了

---
+++ LG_01_函数.py
@@ -0,0 +1,22 @@
+# print("你好")
+# print("挺好玩的")
+#
+# def test1():
+#     print("*" * 50)
+# test1()
+
+def print_line():
+    """
+
+    """
+    row = 1
+
+    while row <=5:
+
+         print("*" * row)
+
+         row +=1
+
+print_line()
+
+
